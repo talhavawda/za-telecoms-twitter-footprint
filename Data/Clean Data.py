fefc39195b85073e38a788e0494220b0d1aa064c
talhavawda <talhavawda.dev@gmail.com>
2020-12-16 19:52:57 +0200

    Added Comment to CleanData

--- Data/Clean Data.py
+++ Data/Clean Data.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
-data_frame = pd.read_csv("keywords.csv")
+"""
+    Tweets from the 4 Companies that are in keywords.csv are duplicates of the ones in data.csv,
+    (they are already  in data.csv) so remove from keywords.csv all tweets that were tweeted by a company 
+"""
+data_frame = pd.read_csv("old_keywords.csv")
 print(data_frame)
 accounts = ["telkomza", "rainsouthafrica", "afrihost", "mtnza"]
 entries=[]
+
 for index, row in data_frame.iterrows():
     username = row['username']
     if username in accounts:
         entries.append(index)
 df = data_frame.drop(entries)
 print(df)
-df.to_csv(r'C:\Users\Ahmad Jawaad Shah\Desktop\Twitter Data\updated_keywords.csv')
+#df.to_csv(r'C:\Users\Ahmad Jawaad Shah\Desktop\Twitter Data\updated_keywords.csv')
+df.to_csv(r'keywords.csv')
