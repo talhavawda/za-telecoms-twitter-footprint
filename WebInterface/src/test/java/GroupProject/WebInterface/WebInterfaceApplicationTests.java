d61c26df57cadefaeed02f3b67f2d949a57b4251
talhavawda <talhavawda.dev@gmail.com>
2020-12-16 21:33:41 +0200

    Created fresh Web Interface

---
+++ WebInterface/src/test/java/GroupProject/WebInterface/WebInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package GroupProject.WebInterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
